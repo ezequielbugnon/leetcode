40e6f72a081fff92bf267405e39bef0edad68e4b
Ezequiel Bugnon <bugnonezequiel@gmail.com>
2025-02-19 15:22:59 -0300

    feat: add implementation graph structure

--- topic/data_structures/graph.go
+++ topic/data_structures/graph.go
@@ -1,27 +1,23 @@
 package datastructures
 
-var ValueArista int = 0
-
-type Arista struct {
-	Value int
+type Edge struct {
+	Value   int
+	Destiny string
 }
 
 type Graph struct {
-	Arista map[string][]Arista
+	Edge map[string][]Edge
 }
 
 func New() *Graph {
 	return &Graph{
-		Arista: make(map[string][]Arista),
+		Edge: make(map[string][]Edge),
 	}
 }
 
 func (g *Graph) AddNode(node string) {
-	if _, ok := g.Arista[node]; !ok {
-		ValueArista++
-		g.Arista[node] = []Arista{
-			{Value: ValueArista},
-		}
+	if _, ok := g.Edge[node]; !ok {
+		g.Edge[node] = []Edge{}
 	}
 }
 
@@ -29,8 +25,30 @@ func (g *Graph) AddVertice(v1, v2 string) {
 	g.AddNode(v1)
 	g.AddNode(v2)
 
-	g.Arista[v1] = append(g.Arista[v1], v2)
-	g.Arista[v2] = append(g.Arista[v2], v1)
+	g.Edge[v1] = append(g.Edge[v1], Edge{
+		Destiny: v2,
+		Value:   1,
+	})
+	g.Edge[v2] = append(g.Edge[v2], Edge{
+		Destiny: v1,
+	})
 }
 
-func (g *Graph) DeleteNode() {}
+func (g *Graph) DeleteNode(node string) {
+	delete(g.Edge, node)
+
+	for k := range g.Edge {
+		g.Edge[k] = g.DeleteEdges(k, node)
+	}
+}
+
+func (g *Graph) DeleteEdges(node string, DeleteEdge string) []Edge {
+	var newEdges []Edge
+	for _, adj := range g.Edge[node] {
+		if DeleteEdge != adj.Destiny {
+			newEdges = append(newEdges, adj)
+		}
+	}
+
+	return newEdges
+}
